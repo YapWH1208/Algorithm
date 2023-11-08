a671f6300b0f1e73ab814c143431fb923d19c07c
YapWH1208 <107160166+YapWH1208@users.noreply.github.com>
2023-11-08 08:48:39 +0800

    Selection Sort

---
+++ SelectionSort.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include "SelectionSort.h"
+
+using namespace std;
+
+void selectionSort(vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 0; i < n - 1; ++i) {
+        // Find the minimum element in the unsorted portion of the array
+        int minIndex = i;
+        for (int j = i + 1; j < n; ++j) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        // Swap the found minimum element with the first element
+        swap(arr[i], arr[minIndex]);
+    }
+}
