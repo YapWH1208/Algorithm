f83b53a69d0c3187585c5dcf31fe7a64dca3e9bc
YapWH1208 <107160166+YapWH1208@users.noreply.github.com>
2023-11-20 22:35:15 +0800

    cmake update

---
+++ cmake-build-debug/CMakeFiles/Algorithm.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/Cpp/Algorithm/BinarySearch.cpp" "CMakeFiles/Algorithm.dir/BinarySearch.cpp.obj" "gcc" "CMakeFiles/Algorithm.dir/BinarySearch.cpp.obj.d"
+  "E:/Cpp/Algorithm/BubbleSort.cpp" "CMakeFiles/Algorithm.dir/BubbleSort.cpp.obj" "gcc" "CMakeFiles/Algorithm.dir/BubbleSort.cpp.obj.d"
+  "E:/Cpp/Algorithm/Main.cpp" "CMakeFiles/Algorithm.dir/Main.cpp.obj" "gcc" "CMakeFiles/Algorithm.dir/Main.cpp.obj.d"
+  "E:/Cpp/Algorithm/MergeSort.cpp" "CMakeFiles/Algorithm.dir/MergeSort.cpp.obj" "gcc" "CMakeFiles/Algorithm.dir/MergeSort.cpp.obj.d"
+  "E:/Cpp/Algorithm/QuickSort.cpp" "CMakeFiles/Algorithm.dir/QuickSort.cpp.obj" "gcc" "CMakeFiles/Algorithm.dir/QuickSort.cpp.obj.d"
+  "E:/Cpp/Algorithm/RadixSort.cpp" "CMakeFiles/Algorithm.dir/RadixSort.cpp.obj" "gcc" "CMakeFiles/Algorithm.dir/RadixSort.cpp.obj.d"
+  "E:/Cpp/Algorithm/SelectionSort.cpp" "CMakeFiles/Algorithm.dir/SelectionSort.cpp.obj" "gcc" "CMakeFiles/Algorithm.dir/SelectionSort.cpp.obj.d"
+  "E:/Cpp/Algorithm/Strassen.cpp" "CMakeFiles/Algorithm.dir/Strassen.cpp.obj" "gcc" "CMakeFiles/Algorithm.dir/Strassen.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Algorithm.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "Algorithm.exe"
+  "Algorithm.exe.manifest"
+  "Algorithm.pdb"
+  "CMakeFiles/Algorithm.dir/BinarySearch.cpp.obj"
+  "CMakeFiles/Algorithm.dir/BinarySearch.cpp.obj.d"
+  "CMakeFiles/Algorithm.dir/BubbleSort.cpp.obj"
+  "CMakeFiles/Algorithm.dir/BubbleSort.cpp.obj.d"
+  "CMakeFiles/Algorithm.dir/Main.cpp.obj"
+  "CMakeFiles/Algorithm.dir/Main.cpp.obj.d"
+  "CMakeFiles/Algorithm.dir/MergeSort.cpp.obj"
+  "CMakeFiles/Algorithm.dir/MergeSort.cpp.obj.d"
+  "CMakeFiles/Algorithm.dir/QuickSort.cpp.obj"
+  "CMakeFiles/Algorithm.dir/QuickSort.cpp.obj.d"
+  "CMakeFiles/Algorithm.dir/RadixSort.cpp.obj"
+  "CMakeFiles/Algorithm.dir/RadixSort.cpp.obj.d"
+  "CMakeFiles/Algorithm.dir/SelectionSort.cpp.obj"
+  "CMakeFiles/Algorithm.dir/SelectionSort.cpp.obj.d"
+  "CMakeFiles/Algorithm.dir/Strassen.cpp.obj"
+  "CMakeFiles/Algorithm.dir/Strassen.cpp.obj.d"
+  "libAlgorithm.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Algorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
