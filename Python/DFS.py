70752c482f12ef138f6991ebcc234599ff56486e
YapWH1208 <107160166+YapWH1208@users.noreply.github.com>
2024-01-21 10:52:53 +0800

    Create DFS.py

---
+++ Python/DFS.py
@@ -0,0 +1,27 @@
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+
+    visited.add(start)
+    print(start, end=" ")
+
+    for neighbor in graph[start]:
+        if neighbor not in visited:
+            dfs(graph, neighbor, visited)
+
+# Example graph represented as an adjacency list
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F', 'G'],
+    'D': ['B'],
+    'E': ['B', 'H'],
+    'F': ['C'],
+    'G': ['C'],
+    'H': ['E']
+}
+
+# Starting vertex for DFS
+start_vertex = 'A'
+print("DFS starting from vertex", start_vertex)
+dfs(graph, start_vertex)
